5fa24fb2338fb83f14bfc7a8f5302657f35c6198
Magnus Nord <magnunor@gmail.com>
2016-07-24 21:12:58 -0400

    Add new testfiles

--- tests/test_find_atomic_columns.py
+++ tests/test_find_atomic_columns.py
@@ -2,12 +2,13 @@ import unittest
 from atomap_tools import get_peak2d_skimage
 from atomap_atom_finding_refining import subtract_average_background, do_pca_on_signal
 from hyperspy.api import load
+import matplotlib.pyplot as plt
 
 class test_finding_columns_skimage(unittest.TestCase):
     
     def setUp(self):
-        s_adf_filename = "tests/datasets/STO_ADF.hdf5"
-        peak_separation = 0.13
+        s_adf_filename = "tests/datasets/test_ADF_cropped.hdf5"
+        peak_separation = 0.15
 
         s_adf = load(s_adf_filename)
         s_adf.change_dtype('float64')
@@ -18,5 +19,5 @@ class test_finding_columns_skimage(unittest.TestCase):
     def test_find_number_of_columns(self):
         peaks = get_peak2d_skimage(
                 self.s_adf_modified, 
-                self.pixel_separation)
-        print(peaks)
+                self.pixel_separation)[0]
+        self.assertEqual(len(peaks), 238)
