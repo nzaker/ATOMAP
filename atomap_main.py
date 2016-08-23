67faf0400647511995fce300e65317375e9d44eb
Magnus Nord <magnunor@gmail.com>
2016-08-23 10:46:23 +0100

    More improvement for the model parameters

--- atomap_main.py
+++ atomap_main.py
@@ -64,6 +64,16 @@ class SrTiO3_110(PerovskiteOxide110):
                 Ti_sublattice_position,
                 O_sublattice_position]
 
+def run_image_filtering(signal, invert_signal=False):
+    signal.change_dtype('float64')
+    signal_modified = subtract_average_background(signal)
+    signal_modified = do_pca_on_signal(signal_modified)
+    signal_modified = normalize_signal(
+            signal_modified, invert_signal=invert_signal)
+    if invert_signal:
+        signal.data = 1./signal.data
+    return(signal_modified)
+
 def run_atom_lattice_peakfinding_process(
         s_adf_filename, 
         s_abf_filename, 
@@ -74,19 +84,10 @@ def run_atom_lattice_peakfinding_process(
     plt.ioff()
     ################
     s_abf = hs.load(s_abf_filename)
-    s_abf.change_dtype('float64')
-    s_abf_modified = subtract_average_background(s_abf)
-    s_abf_modified = do_pca_on_signal(s_abf_modified)
-    s_abf_modified = normalize_signal(
-            s_abf_modified, invert_signal=invert_abf_signal)
-    if invert_abf_signal:
-        s_abf.data = 1./s_abf.data
-    
-    ################
+    s_abf_modified = run_image_filtering(s_abf, invert_signal=True)
+
     s_adf = hs.load(s_adf_filename)
-    s_adf.change_dtype('float64')
-    s_adf_modified = subtract_average_background(s_adf)
-    s_adf_modified = do_pca_on_signal(s_adf_modified)
+    s_adf_modified = run_image_filtering(s_adf)
     
     if model_parameters == "Perovskite110":
         model_parameters = PerovskiteOxide110()
@@ -142,87 +143,6 @@ def run_atom_lattice_peakfinding_process(
     plt.close('all')
     construct_zone_axes_from_sub_lattice(a_sub_lattice)
 
-    zone_vector_100 = a_sub_lattice.zones_axis_average_distances[1]
-    b_atom_list = a_sub_lattice.find_missing_atoms_from_zone_vector(
-            zone_vector_100, new_atom_tag='B')
-
-    b_sub_lattice = Sub_Lattice(
-            b_atom_list, 
-            np.rot90(np.fliplr(s_adf_modified.data)))
-    b_sub_lattice.save_path = "./" + path_name + "/"
-    b_sub_lattice.path_name = path_name
-    b_sub_lattice.plot_color = 'green'
-    b_sub_lattice.tag = 'b'
-    b_sub_lattice.pixel_size = s_adf.axes_manager[0].scale
-    b_sub_lattice.original_adf_image = np.rot90(np.fliplr(s_adf.data))
-    atom_lattice.sub_lattice_list.append(b_sub_lattice)
-
-    for atom in b_sub_lattice.atom_list:
-        atom.sigma_x = 0.03/b_sub_lattice.pixel_size
-        atom.sigma_y = 0.03/b_sub_lattice.pixel_size
-    construct_zone_axes_from_sub_lattice(b_sub_lattice)
-    image_atoms_removed = b_sub_lattice.original_adf_image
-    image_atoms_removed = remove_atoms_from_image_using_2d_gaussian(
-        image_atoms_removed, 
-        a_sub_lattice,
-        percent_distance_to_nearest_neighbor=0.35)
-
-    b_sub_lattice.original_adf_image_atoms_removed =\
-            image_atoms_removed
-    b_sub_lattice.adf_image = image_atoms_removed
-
-    print("Refining b atom lattice")
-    refine_sub_lattice(
-            b_sub_lattice, 
-            [
-                (image_atoms_removed, 2, 'center_of_mass'),
-                (image_atoms_removed, 2, 'gaussian')],
-            0.25)
-
-    plt.close('all')
-
-    zone_vector_110 = b_sub_lattice.zones_axis_average_distances[0]
-    o_atom_list = b_sub_lattice.find_missing_atoms_from_zone_vector(
-            zone_vector_110, new_atom_tag='O')
-
-    o_sub_lattice = Sub_Lattice(
-            o_atom_list, np.rot90(np.fliplr(s_abf_modified.data)))
-    o_sub_lattice.save_path = "./" + path_name + "/"
-    o_sub_lattice.path_name = path_name
-    o_sub_lattice.plot_color = 'red'
-    o_sub_lattice.tag = 'o'
-    o_sub_lattice.pixel_size = s_abf.axes_manager[0].scale
-    o_sub_lattice.original_adf_image = np.rot90(np.fliplr(s_abf.data))
-#    o_sub_lattice.plot_clim = (0.0, 0.2)
-    construct_zone_axes_from_sub_lattice(o_sub_lattice)
-    image_atoms_removed = o_sub_lattice.original_adf_image
-    image_atoms_removed = remove_atoms_from_image_using_2d_gaussian(
-        image_atoms_removed, 
-        a_sub_lattice,
-        percent_distance_to_nearest_neighbor=0.40)
-    image_atoms_removed = remove_atoms_from_image_using_2d_gaussian(
-        image_atoms_removed, 
-        b_sub_lattice,
-        percent_distance_to_nearest_neighbor=0.30)
-
-    o_sub_lattice.adf_image = image_atoms_removed
-
-    for atom in o_sub_lattice.atom_list:
-        atom.sigma_x = 0.025/b_sub_lattice.pixel_size
-        atom.sigma_y = 0.025/b_sub_lattice.pixel_size
-    atom_lattice.sub_lattice_list.append(o_sub_lattice)
-
-    print("Refining o atom lattice")
-    refine_sub_lattice(
-            o_sub_lattice, 
-            [
-                (image_atoms_removed, 1, 'center_of_mass'),
-                (image_atoms_removed, 2, 'gaussian')],
-            0.2)
-
-    atom_lattice.save_atom_lattice()
-
-    plt.close('all')
     plt.ion()
     return(atom_lattice)
 
