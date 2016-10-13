447ed71aad5f46ce2bbf2b6e6cae8f5d009fe313
Magnus Nord <magnunor@gmail.com>
2016-10-13 21:59:04 +0100

    Add setup.py. Move program into atomap folder
    Making it conform to python packaging standards

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+        name = 'atomap'
+        packages = ['atomap']
+        version = '0.01'
+        description = 'Library for analysing atomic resolution images'
+        author = 'Magnus Nord'
+        author_email = 'magnunor@gmail.com'
+        url = 'https://gitlab.com/atomap/atomap'
+)
