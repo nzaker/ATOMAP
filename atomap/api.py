de2b4ce0b30b51cde9f9b0e74494a13aaf01ef07
Magnus Nord <Magnus.Nord@glasgow.ac.uk>
2017-01-31 18:25:59 +0000

    Add api.py, which exposes all the public non-class functions

---
+++ atomap/api.py
@@ -0,0 +1,4 @@
+from atomap.atom_finding_refining import plot_feature_separation
+import hyperspy.api as hs
+from atomap.main import make_atom_lattice_from_image
+from atomap.process_parameters import PerovskiteOxide110
