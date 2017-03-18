f4b8a80c3f40239706b1ccd96f74c365391d6c99
Magnus Nord <magnunor@gmail.com>
2017-03-18 21:57:54 +0000

    Add test for get_feature_separation

--- atomap/tests/test_peak_separation.py
+++ atomap/tests/test_peak_separation.py
@@ -2,7 +2,7 @@ import matplotlib
 matplotlib.use('Agg')
 import os
 import unittest
-from atomap.atom_finding_refining import plot_feature_separation 
+from atomap.atom_finding_refining import get_feature_separation 
 from hyperspy.api import load
 
 my_path = os.path.dirname(__file__)
@@ -10,9 +10,16 @@ my_path = os.path.dirname(__file__)
 class test_peak_separation(unittest.TestCase):
     
     def setUp(self):
-        s_adf_filename = os.path.join(my_path, "datasets", "test_ADF_cropped.hdf5")
+        s_adf_filename = os.path.join(
+                my_path, "datasets", "test_ADF_cropped.hdf5")
         self.s_adf = load(s_adf_filename)
 
     def test_adf(self):
         s_adf = self.s_adf
-        plot_feature_separation(s_adf)
+        signal = get_feature_separation(
+                s_adf,
+                separation_range=(5, 7),
+                separation_step=1,
+                pca=True,
+                subtract_background=True,
+                normalize_intensity=True)
