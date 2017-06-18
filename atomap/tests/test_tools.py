25ec74df5f71eff65a49b1591ba758a856878f68
Magnus Nord <magnunor@gmail.com>
2017-06-18 18:29:08 +0100

    Add test for remove_atoms_from_image_using_2d_gaussian

--- atomap/tests/test_tools.py
+++ atomap/tests/test_tools.py
@@ -2,8 +2,11 @@ import matplotlib
 matplotlib.use('Agg')
 import unittest
 from atomap.tools import array2signal1d, array2signal2d, Fingerprinter
+from atomap.tools import remove_atoms_from_image_using_2d_gaussian
 import atomap.testing_tools as tt
 import numpy as np
+from atomap.atom_finding_refining import get_atom_positions
+from atomap.sublattice import Sublattice
 
 
 class TestArray2Signal(unittest.TestCase):
@@ -52,6 +55,30 @@ class TestFingerprinter(unittest.TestCase):
                         if (xD < tolerance) and (yD < tolerance):
                             bool_list.append(True)
                             break
-
         clusters = (xN*2+1)*(yN*2+1)-1
         self.assertEqual(len(bool_list), clusters)
+
+
+class test_remove_atoms_from_image_using_2d_gaussian(unittest.TestCase):
+
+    def setUp(self):
+        x, y = np.mgrid[0:500:20j,0:500:20j]
+        x, y = x.flatten(), y.flatten()
+        s, g_list = tt.make_artifical_atomic_signal(x, y, image_pad=10)
+        atom_positions = get_atom_positions(
+                signal=s,
+                separation=10,
+                threshold_rel=0.02,
+                )
+        sublattice = Sublattice(
+                atom_position_list=atom_positions,
+                image=s.data)
+        sublattice._find_nearest_neighbors()
+        self.sublattice = sublattice
+
+    def test_running(self):
+        sublattice = self.sublattice
+        subtracted_image = remove_atoms_from_image_using_2d_gaussian(
+                sublattice.image,
+                sublattice,
+                percent_to_nn=0.40)
