a12b49621ec010b186598fad6ed5bd26935cf345
Magnus Nord <magnunor@gmail.com>
2017-06-21 15:00:23 +0100

    Add unit test for Dumb_Lattice.refine_position_gaussian

--- atomap/tests/test_atom_lattice.py
+++ atomap/tests/test_atom_lattice.py
@@ -3,13 +3,17 @@ matplotlib.use('Agg')
 import os
 import unittest
 import numpy as np
-from atomap.atom_lattice import Atom_Lattice
+from atomap.atom_lattice import Atom_Lattice, Dumbbell_Lattice
 from atomap.sublattice import Sublattice
 from atomap.atom_finding_refining import\
         subtract_average_background,\
         do_pca_on_signal,\
         construct_zone_axes_from_sublattice
 from atomap.io import load_atom_lattice_from_hdf5
+from atomap.testing_tools import make_artifical_atomic_signal
+from atomap.initial_position_finding import (
+        find_dumbbell_vector, make_atom_lattice_dumbbell_structure)
+from atomap.atom_finding_refining import get_atom_positions
 from hyperspy.api import load
 
 my_path = os.path.dirname(__file__)
@@ -70,3 +74,31 @@ class test_atom_lattice_object_tools(unittest.TestCase):
                 filename=save_path, overwrite=True)
         self.atom_lattice.save(
                 filename=save_path)
+
+
+class test_dumbbell_lattice(unittest.TestCase):
+
+    def setUp(self):
+        x_list, y_list = [], []
+        for x in range(10, 200, 20):
+            for y in range(10, 200, 20):
+                x_list.append(x)
+                y_list.append(y)
+        for x in range(16, 200, 20):
+            for y in range(10, 200, 20):
+                x_list.append(x)
+                y_list.append(y)
+        sigma_value = 1
+        sigma = [sigma_value]*len(x_list)
+        A = [50]*len(x_list)
+        s, g_list = make_artifical_atomic_signal(
+                x_list, y_list, sigma_x=sigma, sigma_y=sigma, A=A, image_pad=0)
+        vector = find_dumbbell_vector(s, 4)
+        position_list = get_atom_positions(s, separation=13)
+        atom_lattice = make_atom_lattice_dumbbell_structure(
+                s, position_list, vector)
+        self.atom_lattice = atom_lattice
+
+    def test_refine_position_gaussian(self):
+        atom_lattice = self.atom_lattice
+        atom_lattice.refine_position_gaussian()
