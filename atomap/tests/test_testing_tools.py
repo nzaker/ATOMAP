61e86b75b0fd4410bc3d4d679d6bd0883bbee181
Magnus Nord <magnunor@gmail.com>
2017-10-01 12:12:09 +0100

    Add tests for MakeTestData class

--- atomap/tests/test_testing_tools.py
+++ atomap/tests/test_testing_tools.py
@@ -6,6 +6,43 @@ import atomap.testing_tools as tt
 from numpy import mgrid
 
 
+class TestMakeTestData(unittest.TestCase):
+
+    def test_simple_init(self):
+        tt.MakeTestData(100, 100)
+
+    def test_get_signal(self):
+        imX0, imY0 = 100, 100
+        test_data0 = tt.MakeTestData(imX0, imY0)
+        self.assertEqual(imX0, test_data0.image_x)
+        self.assertEqual(imY0, test_data0.image_y)
+        self.assertEqual(test_data0.signal.axes_manager.shape, (imX0, imY0))
+
+        imX1, imY1 = 100, 39
+        test_data1 = tt.MakeTestData(imX1, imY1)
+        self.assertEqual(imX1, test_data1.image_x)
+        self.assertEqual(imY1, test_data1.image_y)
+        self.assertEqual(test_data1.signal.axes_manager.shape, (imX1, imY1))
+
+        imX2, imY2 = 34, 65
+        test_data2 = tt.MakeTestData(imX2, imY2)
+        self.assertEqual(imX2, test_data2.image_x)
+        self.assertEqual(imY2, test_data2.image_y)
+        self.assertEqual(test_data2.signal.axes_manager.shape, (imX2, imY2))
+
+    def test_add_atom(self):
+        x, y, sx, sy, A, r = 10, 5, 5, 9, 10, 2
+        td = tt.MakeTestData(50, 50)
+        td.add_atom(x, y, sigma_x=sx, sigma_y=sy, amplitude=A, rotation=r)
+        self.assertEqual(len(td.sublattice.atom_list), 1)
+        atom = td.sublattice.atom_list[0]
+        self.assertEqual(atom.pixel_x, x)
+        self.assertEqual(atom.pixel_y, y)
+        self.assertEqual(atom.sigma_x, sx)
+        self.assertEqual(atom.sigma_y, sy)
+        self.assertEqual(atom.amplitude_gaussian, A)
+        self.assertEqual(atom.rotation, r)
+
 class TestMakeArtificalAtomicSignal(unittest.TestCase):
     def setUp(self):
         x, y = mgrid[0:50:5j,0:50:5j]
