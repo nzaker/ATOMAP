e3a99f5d77d09f5926382f142cb5c43275b04856
Magnus Nord <magnunor@gmail.com>
2017-10-07 11:26:35 +0100

    doc: remove bizstyle from conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -16,7 +16,6 @@
 import sys
 import os
 import sphinx_rtd_theme
-import sphinxjp.themes.bizstyle as bizstyle
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -120,7 +119,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 html_theme = 'sphinx_rtd_theme'
-#sphinx_rtd_theme,bizstyle 
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
