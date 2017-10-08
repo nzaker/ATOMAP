1064e0a145628dc7dbc4ed7147cdf48ae3205ce5
Ida <idahjorth@gmail.com>
2017-10-08 11:22:17 +0100

    TutorialImages: New automatic plotting scripts fig

---
+++ doc/images/001_finding_atom_lattices_images_sc.py
@@ -0,0 +1,69 @@
+import atomap.api as am
+import numpy as np
+import atomap.dummy_data as dummy_data
+
+def save_simple_cubic_image(s):
+    s.plot()
+    s._plot.signal_plot.figure.savefig(
+            'finding_atom_lattices/sc_image.png', overwrite=True)
+
+def save_s_peaks_image(s):
+    s_peaks = am.get_feature_separation(s, separation_range=(2, 20))
+    s_peaks.plot()
+    s_peaks._plot.signal_plot.figure.savefig(
+                   'finding_atom_lattices/peak_finding_1a.png',overwrite=True)
+    s_peaks._plot.navigator_plot.figure.savefig(
+                   'finding_atom_lattices/peak_finding_1b.png',overwrite=True)
+    s_peaks.axes_manager.indices = (5,)
+    s_peaks._plot.signal_plot.figure.savefig(
+                   'finding_atom_lattices/peak_finding_2a.png',overwrite=True)
+    s_peaks._plot.navigator_plot.figure.savefig(
+                   'finding_atom_lattices/peak_finding_2b.png',overwrite=True)
+    s_peaks.axes_manager.indices = (10,)
+    s_peaks._plot.signal_plot.figure.savefig(
+                   'finding_atom_lattices/peak_finding_3a.png',overwrite=True)
+    s_peaks._plot.navigator_plot.figure.savefig(
+                   'finding_atom_lattices/peak_finding_3b.png',overwrite=True)
+
+def plot_position_history(sublattice):
+    s = sublattice.get_position_history()
+    s.plot()
+    s._plot.signal_plot.figure.savefig(
+                   'finding_atom_lattices/pos_hist_1a.png',overwrite=True)
+    s._plot.navigator_plot.figure.savefig(
+                   'finding_atom_lattices/pos_hist_1b.png',overwrite=True)
+    s.axes_manager.indices = (2,)
+    s._plot.signal_plot.figure.savefig(
+                   'finding_atom_lattices/pos_hist_2a.png',overwrite=True)
+    s._plot.navigator_plot.figure.savefig(
+                   'finding_atom_lattices/pos_hist_2b.png',overwrite=True)
+    s._plot.signal_plot.ax.set_xlim(105,180)
+    s._plot.signal_plot.ax.set_ylim(105,180)
+    s._plot.signal_plot.figure.savefig(
+                   'finding_atom_lattices/pos_hist_2_zoom.png',overwrite=True)
+
+def plot_planes_figure(sublattice):
+    s = sublattice.get_all_atom_planes_by_zone_vector()
+    s.plot()
+    s.axes_manager.indices = (1,)
+    s._plot.signal_plot.figure.savefig(
+                   'finding_atom_lattices/zone_axes_sig.png',overwrite=True)
+    s._plot.navigator_plot.figure.savefig(
+                   'finding_atom_lattices/zone_axes_nav.png',overwrite=True)
+
+                      
+s = dummy_data.get_simple_cubic_signal(image_noise=True)
+
+save_simple_cubic_image(s)
+save_s_peaks_image(s)
+
+atom_positions = am.get_atom_positions(s, separation=7)
+sublattice = am.Sublattice(atom_positions, image=s.data)
+sublattice.find_nearest_neighbors()
+sublattice.refine_atom_positions_using_center_of_mass()
+sublattice.refine_atom_positions_using_2d_gaussian()
+
+plot_position_history(sublattice)
+
+sublattice.construct_zone_axes()
+plot_planes_figure(sublattice)
---
+++ doc/images/003_make_fantasite.py
@@ -0,0 +1,35 @@
+import atomap.api as am
+from atomap.tools import remove_atoms_from_image_using_2d_gaussian
+
+def plot_fantasite(s):
+    s.plot()
+    s._plot.signal_plot.figure.savefig(
+            'plotting_tutorial/fantasite.png', overwrite=True)
+
+def plot_atom_lattice(atom_lattice):
+    s = atom_lattice.get_sublattice_atom_list_on_image()
+    s.plot()
+    s._plot.signal_plot.figure.savefig(
+            'plotting_tutorial/atom_lattice.png', overwrite=True)
+
+s = am.dummy_data.get_fantasite()
+A_positions = am.get_atom_positions(s,separation=12)
+sublattice_A = am.Sublattice(A_positions,image=s.data, color='r', name='A')
+sublattice_A.find_nearest_neighbors()
+sublattice_A.refine_atom_positions_using_center_of_mass()
+sublattice_A.refine_atom_positions_using_2d_gaussian()
+sublattice_A.construct_zone_axes()
+
+direction_001 = sublattice_A.zones_axis_average_distances[1]
+B_positions = sublattice_A._find_missing_atoms_from_zone_vector(direction_001)
+image_without_A = remove_atoms_from_image_using_2d_gaussian(sublattice_A.image, sublattice_A)
+
+sublattice_B = am.Sublattice(B_positions, image_without_A, color='blue', name='B')
+sublattice_B.construct_zone_axes()
+sublattice_B.refine_atom_positions_using_center_of_mass()
+sublattice_B.refine_atom_positions_using_2d_gaussian()
+atom_lattice = am.Atom_Lattice(image=s.data, name='fantasite', sublattice_list=[sublattice_A, sublattice_B])
+atom_lattice.save("fantasite.hdf5", overwrite=True)
+
+plot_fantasite(s)
+plot_atom_lattice(atom_lattice)
