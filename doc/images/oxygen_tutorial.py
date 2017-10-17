3b5090095152d773a147a3093e405a956551e4ea
Ida <idahjorth@gmail.com>
2017-10-17 17:11:35 +0100

    Add script for making oxygen tutorial images

---
+++ doc/images/oxygen_tutorial.py
@@ -0,0 +1,85 @@
+import atomap.api as am
+import numpy as np
+from atomap.tools import remove_atoms_from_image_using_2d_gaussian
+import matplotlib.pyplot as plt
+
+# Loading signals
+s_ADF = am.dummy_data.get_two_sublattice_signal()
+s_ABF = am.dummy_data.get_perovskite110_ABF_signal(image_noise=True)
+
+# Plot ABF signal
+s_ABF.plot()
+plt.gcf().savefig('finding_atom_lattices/s_ABF.png')
+
+# Finding sublattice_A from ADF
+A_positions = am.get_atom_positions(s_ADF, separation=15)
+sublattice_A = am.Sublattice(A_positions, image=s_ADF.data, color='r')
+sublattice_A.find_nearest_neighbors()
+sublattice_A.refine_atom_positions_using_center_of_mass()
+sublattice_A.refine_atom_positions_using_2d_gaussian()
+sublattice_A.construct_zone_axes()
+
+# Finding sublattice_B from ADF
+zone_axis_001 = sublattice_A.zones_axis_average_distances[1]
+B_positions = sublattice_A._find_missing_atoms_from_zone_vector(zone_axis_001)
+image_without_A = remove_atoms_from_image_using_2d_gaussian(
+        sublattice_A.image,
+        sublattice_A)
+sublattice_B = am.Sublattice(B_positions, image_without_A, color='blue')
+sublattice_B.construct_zone_axes()
+sublattice_B.refine_atom_positions_using_center_of_mass()
+sublattice_B.refine_atom_positions_using_2d_gaussian()
+
+# Finding sublattice_A2 in ABF for subtraction
+xy = np.asarray(sublattice_A.atom_positions)
+xy = np.swapaxes(xy, 0, 1)
+sublattice_A2 = am.Sublattice(xy, image=np.divide(1, s_ABF.data), color='r')
+sublattice_A2.find_nearest_neighbors()
+sublattice_A2.refine_atom_positions_using_center_of_mass()
+sublattice_A2.refine_atom_positions_using_2d_gaussian()
+sublattice_A2.construct_zone_axes()
+
+# Finding sublattice_B2 in ABF for subtraction
+image_without_A2 = remove_atoms_from_image_using_2d_gaussian(
+        sublattice_A2.image,
+        sublattice_A2)
+xy = np.asarray(sublattice_B.atom_positions)
+xy = np.swapaxes(xy, 0, 1)
+sublattice_B2 = am.Sublattice(xy, image=image_without_A2, color='b')
+sublattice_B2.find_nearest_neighbors()
+sublattice_B2.refine_atom_positions_using_center_of_mass()
+sublattice_B2.refine_atom_positions_using_2d_gaussian()
+sublattice_B2.construct_zone_axes()
+
+sublattice_B2.plot_planes()
+plt.gcf().savefig('finding_atom_lattices/sublattice_B2.png')
+
+# Finding Oxygen
+zone_axis_002 = sublattice_B2.zones_axis_average_distances[0]
+O_positions = sublattice_B2._find_missing_atoms_from_zone_vector(zone_axis_002)
+image_without_AB = remove_atoms_from_image_using_2d_gaussian(
+        sublattice_B2.image,
+        sublattice_B2)
+
+sublattice_O = am.Sublattice(O_positions, image_without_AB, color='g')
+sublattice_O.construct_zone_axes()
+sublattice_O.refine_atom_positions_using_center_of_mass()
+sublattice_O.refine_atom_positions_using_2d_gaussian()
+
+s = sublattice_O.get_atom_list_on_image()
+s.plot()
+s._plot.signal_plot.ax.set_xlim(70, 130)
+s._plot.signal_plot.ax.set_ylim(50, 100)
+s._plot.signal_plot.figure.savefig(
+    'finding_atom_lattices/oxygen_positions.png',
+    overwrite=True)
+
+atom_lattice = am.Atom_Lattice(
+                image=s_ABF.data, name='ABO3',
+                sublattice_list=[sublattice_A, sublattice_B, sublattice_O])
+
+atom_lattice.plot()
+plt.gcf().savefig('finding_atom_lattices/ABO3.png')
+s = atom_lattice.get_sublattice_atom_list_on_image(image=s_ADF.data)
+s.plot()
+plt.gcf().savefig('finding_atom_lattices/ABO3-ADF.png')
