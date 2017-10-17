f95bb36cb80e62c378cb853e6cdc58833af87b22
Ida <idahjorth@gmail.com>
2017-10-17 17:39:48 +0100

    Add script for making plots for testdata tutorial

---
+++ doc/images/test_data_figures.py
@@ -0,0 +1,33 @@
+import atomap.api as am
+import matplotlib.pyplot as plt
+import atomap.testing_tools as tt
+import numpy as np
+
+s = am.dummy_data.get_dumbbell_signal()
+s.plot()
+plt.gcf().savefig('testdata/dumbbell.png')
+
+t1 =  tt.MakeTestData(20,20)
+t1.add_atom(10,10)
+t1.signal.plot()
+plt.gcf().savefig('testdata/t1.png')
+
+
+t2 = tt.MakeTestData(200,200)
+x, y = np.mgrid[0:200:10j, 0:200:10j]
+x, y = x.flatten(), y.flatten()
+t2.add_atom_list(x, y)
+t2.signal.plot()
+plt.gcf().savefig('testdata/t2.png')
+
+t3 = tt.MakeTestData(200,200)
+x, y = np.mgrid[0:200:20, 0:200:20]
+x, y = x.flatten(), y.flatten()
+t3.add_atom_list(x, y,sigma_x=2, sigma_y=1.5, amplitude=20, rotation=0.4)
+
+x, y = np.mgrid[10:200:20, 10:200:20]
+x, y = x.flatten(), y.flatten()
+t3.add_atom_list(x, y,sigma_x=2, sigma_y=2, amplitude=40)
+t3.add_image_noise(sigma=0.1)
+t3.signal.plot()
+plt.gcf().savefig('testdata/t3.png')
