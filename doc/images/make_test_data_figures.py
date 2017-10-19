2c4ecae2e18b5fb90d371b44d1deca170a38df4b
Ida <idahjorth@gmail.com>
2017-10-19 10:57:56 +0100

    Change name from testdata to maketestdata

--- doc/images/make_test_data_figures.py
+++ doc/images/make_test_data_figures.py
@@ -5,12 +5,12 @@ import numpy as np
 
 s = am.dummy_data.get_dumbbell_signal()
 s.plot()
-plt.gcf().savefig('testdata/dumbbell.png')
+plt.gcf().savefig('maketestdata/dumbbell.png')
 
 t1 =  tt.MakeTestData(20,20)
 t1.add_atom(10,10)
 t1.signal.plot()
-plt.gcf().savefig('testdata/t1.png')
+plt.gcf().savefig('maketestdata/t1.png')
 
 
 t2 = tt.MakeTestData(200,200)
@@ -18,7 +18,7 @@ x, y = np.mgrid[0:200:10j, 0:200:10j]
 x, y = x.flatten(), y.flatten()
 t2.add_atom_list(x, y)
 t2.signal.plot()
-plt.gcf().savefig('testdata/t2.png')
+plt.gcf().savefig('maketestdata/t2.png')
 
 t3 = tt.MakeTestData(200,200)
 x, y = np.mgrid[0:200:20, 0:200:20]
@@ -30,4 +30,4 @@ x, y = x.flatten(), y.flatten()
 t3.add_atom_list(x, y,sigma_x=2, sigma_y=2, amplitude=40)
 t3.add_image_noise(sigma=0.1)
 t3.signal.plot()
-plt.gcf().savefig('testdata/t3.png')
+plt.gcf().savefig('maketestdata/t3.png')
