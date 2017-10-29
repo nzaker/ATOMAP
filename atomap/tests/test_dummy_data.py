0fb410fed3579936bc8d10238c6dd42abbf6b9a4
Magnus Nord <magnunor@gmail.com>
2017-10-29 16:18:44 +0000

    Unit tests: move fantasite tests to its own test class.
    Add test for the atom lattice

--- atomap/tests/test_dummy_data.py
+++ atomap/tests/test_dummy_data.py
@@ -41,16 +41,6 @@ class test_dummy_data(unittest.TestCase):
         s = dd.get_dumbbell_signal()
         s.plot()
 
-    def test_get_fantasite(self):
-        s = dd.get_fantasite()
-        s.plot()
-        s1 = dd.get_fantasite()
-        self.assertTrue((s.data == s1.data).all())
-
-    def test_get_fantasite_sublattice(self):
-        s = dd.get_fantasite_sublattice()
-        s.plot()
-
     def test_get_perovskite110_ABF_signal(self):
         s0 = dd.get_perovskite110_ABF_signal()
         s0.plot()
@@ -66,3 +56,21 @@ class test_dummy_data(unittest.TestCase):
         s1.plot()
         s2 = dd.get_simple_atom_lattice_two_sublattices(image_noise=False)
         s2.plot()
+
+
+class dummy_data_fantasite(unittest.TestCase):
+
+    def test_signal(self):
+        s = dd.get_fantasite()
+        s.plot()
+        s1 = dd.get_fantasite()
+        self.assertTrue((s.data == s1.data).all())
+
+    def test_sublattice(self):
+        sublattice = dd.get_fantasite_sublattice()
+        self.assertEqual(
+                len(sublattice.x_position), len(sublattice.y_position))
+
+    def test_atom_lattice(self):
+        atom_lattice = dd.get_fantasite_atom_lattice()
+        self.assertEqual(len(atom_lattice.sublattice_list), 2)
