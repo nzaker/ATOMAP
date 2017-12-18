0654f12484ac5dbe8db9ff5149cf41abe9b3041f
Magnus Nord <magnunor@gmail.com>
2017-12-18 07:10:12 +0100

    Add new atom_plane_tolerance argument to construct_zone_axes
    This also includes the methods related to constructing the atom planes

--- atomap/sublattice.py
+++ atomap/sublattice.py
@@ -1092,15 +1092,18 @@ class Sublattice():
                 temp_atom_plane_list.append(atom_plane)
         return(temp_atom_plane_list)
 
-    def _generate_all_atom_plane_list(self):
+    def _generate_all_atom_plane_list(self, atom_plane_tolerance=0.5):
         for zone_vector in self.zones_axis_average_distances:
-            self._find_all_atomic_planes_from_direction(zone_vector)
+            self._find_all_atomic_planes_from_direction(
+                    zone_vector, atom_plane_tolerance=atom_plane_tolerance)
 
-    def _find_all_atomic_planes_from_direction(self, zone_vector):
+    def _find_all_atomic_planes_from_direction(
+            self, zone_vector, atom_plane_tolerance=0.5):
         for atom in self.atom_list:
             if not atom.is_in_atomic_plane(zone_vector):
                 atom_plane = self._find_atomic_columns_from_atom(
-                        atom, zone_vector)
+                        atom, zone_vector,
+                        atom_plane_tolerance=atom_plane_tolerance)
                 if not (len(atom_plane) == 1):
                     atom_plane_instance = Atom_Plane(
                             atom_plane, zone_vector, self)
@@ -1109,8 +1112,8 @@ class Sublattice():
                     self.atom_plane_list.append(atom_plane_instance)
 
     def _find_atomic_columns_from_atom(
-            self, start_atom, zone_vector, atom_range_factor=0.5):
-        atom_range = atom_range_factor*self._pixel_separation
+            self, start_atom, zone_vector, atom_plane_tolerance=0.5):
+        atom_range = atom_plane_tolerance*self._pixel_separation
         end_of_atom_plane = False
         zone_axis_list1 = [start_atom]
         while not end_of_atom_plane:
@@ -1995,7 +1998,8 @@ class Sublattice():
         mean_angle = np.array(angle_list).mean()
         return(mean_angle)
 
-    def construct_zone_axes(self, zone_axis_para_list=False):
+    def construct_zone_axes(
+            self, atom_plane_tolerance=0.5, zone_axis_para_list=False):
         """Constructs the zone axes for an atomic resolution image.
 
         The zone axes are found by finding the 15 nearest neighbors for each
@@ -2007,6 +2011,12 @@ class Sublattice():
 
         Parameters
         ----------
+        atom_plane_tolerance : scalar, default 0.5
+            When constructing the atomic planes, the method will try to locate
+            the atoms by "jumping" one zone vector, and seeing if there is an
+            atom with the pixel_separation times atom_plane_tolerance. So this
+            value should be increased the atomic planes are non-continuous and
+            "split".
         zone_axis_para_list : parameter list or bool, default False
             A zone axes parameter list is used to name and index the zone
             axes. See atomap.process_parameters for more info. Useful for
@@ -2028,7 +2038,8 @@ class Sublattice():
 
         """
         construct_zone_axes_from_sublattice(
-                self, zone_axis_para_list=zone_axis_para_list)
+                self, atom_plane_tolerance=atom_plane_tolerance,
+                zone_axis_para_list=zone_axis_para_list)
 
     def _get_fingerprint(self, pixel_radius=100):
         """
