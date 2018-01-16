0747df88496e7aedff33a9cf8cec4c91353905ae
k8macarthur <k8macarthur@gmail.com>
2018-01-16 17:06:01 +0100

    ...

--- atomap/tests/test_tools.py
+++ atomap/tests/test_tools.py
@@ -100,7 +100,7 @@ class test_adf_quantification(unittest.TestCase):
                        95, 95, 95, 95, 95, 95, 95, 95, 95]
 
     def test_running(self):
-        result = Integrate(self.test_data, x_positions, y_positions)
+        result = Integrate(self.test_data, self.x_positions, self.y_positions)
         np.testing.assert_allclose(np.sum(result[0]),
                                    np.sum(self.test_data),
                                    atol=1e-3)
