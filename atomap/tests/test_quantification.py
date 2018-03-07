27b139a14f33d1bafd570984853e01cc9e66ecc3
k8macarthur <k8macarthur@gmail.com>
2018-03-07 17:13:45 +0100

    Typo fixing.

--- atomap/tests/test_quantification.py
+++ atomap/tests/test_quantification.py
@@ -3,6 +3,7 @@ import unittest
 import numpy as np
 import math
 import atomap.quantification as quant
+from atomap.example import get_detector_image_signal
 #from hyperspy.signals import Signal2D
 #import atomap.dummy_data as dd
 #import atomap.testing_tools as tt
@@ -16,22 +17,22 @@ class TestDetectorNormalisation(unittest.TestCase):
         self.assertEqual(s[63, 63], 44.553388, atol=1E-3)
 
     def test_detector_threshold(self):
-        det_image = am.example_data.get_detector_image_signal()
+        det_image = get_detector_image_signal()
         threshold_image = quant._detector_threshold(det_image.date)
         self.assertFalse(np.sum(threhold_image), 0)
         self.assertEqual(det_image.data.shape, threshold_image.shape)
 
     def test_radial_profile(self):
-        det_image = am.example_data.get_detector_image_signal()
+        det_image = get_detector_image_signal()
         profile = quant._radial_profile(det_image.data, (256, 256))
         self.assertEqual(len(np.shape(profile)), 1)
         self.assertEqual(np.shape(profile)[0], math.ceil(math.sqrt(2) * 256))
 
     def test_detector_normalisation(self):
-        det_image = am.example_data.get_detector_image_signal()
+        det_image = .get_detector_image_signal()
         img = am.dummy_data.get_simple_cubic_signal(image_noise=True)
         img = (img) * 300000 + 4000
-        image_normalised = am.quant.detector_normalisation(img, det_image, 60)
+        image_normalised = quant.detector_normalisation(img, det_image, 60)
         self.assertTrue(image_normalised.data.max() < 1)
         self.assertEqual(image_normalised.data.shape, img.data.shape)
 
