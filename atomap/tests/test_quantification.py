260b13128713d867775d294aedf5e4f008abe8c9
Magnus Nord <Magnus.Nord@glasgow.ac.uk>
2018-03-20 16:55:38 +0000

    test_quantification: change from unittest to pytest
    Currently test_func is failing, due to some wrong value comparison?

--- atomap/tests/test_quantification.py
+++ atomap/tests/test_quantification.py
@@ -1,4 +1,3 @@
-import unittest
 import numpy as np
 import math
 import atomap.quantification as quant
@@ -6,38 +5,38 @@ from atomap.example_data import get_detector_image_signal
 from atomap.dummy_data import get_simple_cubic_signal
 
 
-class TestDetectorNormalisation(unittest.TestCase):
+class TestDetectorNormalisation:
 
     def test_centered_distance_matrix(self):
         s = quant.centered_distance_matrix((32, 32), np.zeros((64, 64)))
-        self.assertEqual(s[32, 32], 1)
-        self.assertEqual(s[63, 63], np.sqrt((63-31)**2 + (63-32)**2))
+        assert s[32, 32] == 1
+        assert s[63, 63] == np.sqrt((63-31)**2 + (63-32)**2)
 
     def test_detector_threshold(self):
         det_image = get_detector_image_signal()
         threshold_image = quant._detector_threshold(det_image.data)
-        self.assertNotEqual(np.sum(threshold_image), 0)
-        self.assertEqual(det_image.data.shape, threshold_image.shape)
+        assert not (np.sum(threshold_image) == 0)
+        assert det_image.data.shape == threshold_image.shape
 
     def test_radial_profile(self):
         det_image = get_detector_image_signal()
         profile = quant._radial_profile(det_image.data, (256, 256))
-        self.assertEqual(len(np.shape(profile)), 1)
-        self.assertEqual(np.shape(profile)[0], math.ceil(math.sqrt(2) * 256))
+        assert len(np.shape(profile)) == 1
+        assert np.shape(profile)[0] == math.ceil(math.sqrt(2) * 256)
 
     def test_detector_normalisation(self):
         det_image = get_detector_image_signal()
         img = get_simple_cubic_signal(image_noise=True)
         img = (img) * 300000 + 4000
         image_normalised = quant.detector_normalisation(img, det_image, 60)
-        self.assertTrue(image_normalised.data.max() < 1)
-        self.assertEqual(image_normalised.data.shape, img.data.shape)
+        assert image_normalised.data.max() < 1
+        assert image_normalised.data.shape == img.data.shape
 
     def test_func(self):
         result = quant._func(2, 4, 0.5, 5)
-        self.assertTrue(result, 6)
+        assert result == 6
 
     def test_find_flux_limits_running(self):
         flux1 = quant.centered_distance_matrix((63, 63), np.zeros((128, 128)))
-        (profiler, flux_profile) = quant.find_flux_limits(100-flux1, 25)
-        self.assertTrue(len(flux_profile), math.ceil((64**2+64**2)**0.5))
+        (profiler, flux_profile) = quant.find_flux_limits(100 - flux1, 25)
+        assert len(flux_profile) == math.ceil((64**2 + 64**2)**0.5)
