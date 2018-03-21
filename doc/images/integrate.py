47c53449f7bec67dd200f9552dacfd61459f7f51
Magnus Nord <Magnus.Nord@glasgow.ac.uk>
2018-03-21 13:56:39 +0000

    doc: Flake8 fixes for doc/images/integrate.py

--- doc/images/integrate.py
+++ doc/images/integrate.py
@@ -1,16 +1,18 @@
 import os
-import numpy as np
 import atomap.api as am
 import atomap.dummy_data as dummy_data
 import hyperspy.api as hs
+
 
 my_path = os.path.dirname(__file__) + '/integrate/'
 if not os.path.exists(my_path):
     os.makedirs(my_path)
 
+
 def voronoi_integration(sublattice):
     s = dummy_data.get_simple_cubic_signal(image_noise=True)
-    s_integrated = am.integrate(s.data, sublattice.x_position, sublattice.y_position)
+    s_integrated = am.integrate(
+            s.data, sublattice.x_position, sublattice.y_position)
     s2 = hs.signals.Signal2D(s_integrated[2])
     s2.plot(cmap='viridis')
     s2._plot.signal_plot.figure.savefig('Voronoi1.png', overwrite=True)
@@ -18,9 +20,12 @@ def voronoi_integration(sublattice):
     s2.plot(cmap='viridis')
     s2._plot.signal_plot.figure.savefig('Voronoi2.png', overwrite=True)
 
+
 def watershed_integration(sublattice):
     s = dummy_data.get_simple_cubic_signal(image_noise=True)
-    s_integrated = am.integrate(s.data, sublattice.x_position, sublattice.y_position, method='Watershed')
+    s_integrated = am.integrate(
+            s.data, sublattice.x_position, sublattice.y_position,
+            method='Watershed')
     s2 = hs.signals.Signal2D(s_integrated[2])
     s2.plot(cmap='viridis')
     s2._plot.signal_plot.figure.savefig('Watershed1.png', overwrite=True)
