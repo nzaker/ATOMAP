86e0e4e5311f81d9d71ebf85953d75b5754a03b8
Magnus Nord <magnunor@gmail.com>
2020-01-29 15:53:04 +0100

    Update version to development version 0.2.2.dev1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
             'atomap.external',
             'atomap.example_data',
             ],
-        version = '0.2.1',
+        version = '0.2.2.dev1',
         description = 'Library for analysing atomic resolution images',
         long_description=long_description,
         long_description_content_type='text/markdown',
